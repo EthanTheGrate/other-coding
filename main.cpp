9baec495e9e5c8c0bb3fe81af08ac5ab257479c8
EthanTheGrate <137196080+EthanTheGrate@users.noreply.github.com>
2023-07-14 13:18:10 -0600

    c++ file
    woohoo

---
+++ main.cpp
@@ -0,0 +1,87 @@
+/*
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+    
+    return 0;
+}
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+    string item = "chair";
+    string *pointer = &item;
+    *pointer = "table";
+    cout<<item<<"\n";
+    cout<<pointer<<"\n";
+    cout<<*pointer;
+    return 0;
+}
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+void function()
+{
+    cout<<"lorem ipsum";
+}
+int main()
+{
+    function();
+    return 0;
+}
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+void function(int x, int y)
+{
+    cout<<"text "<<x<<" more text "<<y;
+}
+int main()
+{
+    function(2, 3);
+    cout<<"\n";
+    function(4, 5);
+    cout<<"\n";
+    function(6, 7);
+}
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+void function(string name, int age)
+{
+    cout<<name<<": "<<age<<" years old\n";
+}
+int main()
+{
+    function("Ethan", 13);
+    function("Old Ethan", 69);
+}
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+int sum(int x)
+{
+    if(x > 0)
+    {
+        return x + sum(x-2);
+    }
+    else
+    {
+        return 0;
+    }
+}
+int main()
+{
+    int ans = sum(5);
+    cout<<ans;
+    return 0;
+}
+*/
